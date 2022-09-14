caa4fcaa764c9c774c91951291cd7d50ce5d5525
mig <michaelleojacob@gmail.com>
2022-09-13 19:58:08 -0700

    got the initial routes set up, and the index.js boilerplate

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.DB_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+  )
+  .catch((err) => console.error(err.message));
